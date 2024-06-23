c0b27ed3c06e43c95406854e330d6e60bc923a1c
Nathan Vogt <nathancvogt@gmail.com>
2024-06-22 20:18:37 -0700

    I hate CORS

--- app/src/home.tsx
+++ app/src/home.tsx
@@ -1,11 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 type Props = {
   targetImage: File;
 };
 
 export function Home({ targetImage }: Props) {
+  const program = "(+ (- (Circle 8 8 8) (Circle 5 8 8)) (Quad 8 8 4 4 H))";
   const imageUrl = URL.createObjectURL(targetImage);
+
+  useEffect(() => {
+    function listenToServerEvents(): void {
+      const source = new EventSource("http://localhost:5000");
+
+      source.onmessage = function (event) {
+        console.log("Received data:", event.data);
+      };
+
+      source.onerror = function (event) {
+        console.error("EventSource failed:", event);
+        source.close();
+      };
+
+      window.onunload = function () {
+        source.close();
+      };
+    }
+
+    listenToServerEvents();
+  }, []);
 
   useEffect(() => {
     return () => {
@@ -14,13 +36,39 @@ export function Home({ targetImage }: Props) {
   }, [imageUrl]);
 
   return (
-    <div style={{ display: "flex", width: "100vw", height: "100vh" }}>
-      <div style={{ width: "33.33%", border: "2px solid red", height: "100%" }}>
+    <div style={{ display: "flex", width: "100%", height: "100vh" }}>
+      <div
+        style={{
+          width: "33.33%",
+          border: "2px solid red",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <img src={imageUrl} alt="Uploaded Target" style={{ width: "100%" }} />
       </div>
       <div
-        style={{ width: "33.33%", height: "100%", border: "2px solid green" }}
-      ></div>
+        style={{
+          width: "33.33%",
+          height: "100%",
+          border: "2px solid green",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "20px",
+          overflow: "auto",
+          fontFamily: "'Courier New', monospace",
+          fontSize: "16px",
+          textAlign: "center",
+          whiteSpace: "pre-wrap",
+          backgroundColor: "#f4f4f4",
+          borderRadius: "8px",
+        }}
+      >
+        {program}
+      </div>
       <div
         style={{ width: "33.33%", height: "100%", border: "2px solid blue" }}
       ></div>
